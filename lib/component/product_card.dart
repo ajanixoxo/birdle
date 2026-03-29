4916db87d3d9bdc7749643b8adb7574632e3fc4c
Adeoluwa Gbenro <joelayomide35@gmail.com>
2026-03-29 04:39:03 +0100

    Update product card layout and enhance home screen with additional product details

--- lib/component/product_card.dart
+++ lib/component/product_card.dart
@@ -2,103 +2,139 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../constant/constant.dart';
 
-
 class ProductCard extends StatelessWidget {
   final Map<String, String> product;
 
-  const ProductCard({
-    super.key,
-    required this.product,
-  });
+  const ProductCard({super.key, required this.product});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(16),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.07),
-            blurRadius: 8,
-            offset: Offset(0, 4),
+    return Stack(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            border: Border.all(color: AppColors.textDark),
           ),
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // image
-          ClipRRect(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(16),
-              topRight: Radius.circular(16),
-            ),
-            child: Image.asset(
-              product["image"]!,
-              width: double.infinity,
-              height: 120,
-              fit: BoxFit.cover,
-            ),
-          ),
-
-          // content
-          Padding(
-            padding: EdgeInsets.all(10),
+          child: Padding(
+            padding: EdgeInsets.all(13),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              spacing: 4,
               children: [
-                Text(
-                  product["name"]!,
-                  style: GoogleFonts.poppins(
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
+                // image
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(16),
+                    topRight: Radius.circular(16),
                   ),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                Text(
-                  product["category"]!,
-                  style: GoogleFonts.poppins(
-                    fontSize: 12,
-                    color: Colors.grey,
+                  child: Image.asset(
+                    product["image"]!,
+                    width: double.infinity,
+                    height: 120,
+                    fit: BoxFit.cover,
                   ),
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      product["price"]!,
-                      style: GoogleFonts.poppins(
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold,
-                        color: AppColors.primary,
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: () {},
-                      child: Container(
-                        width: 30,
-                        height: 30,
-                        decoration: BoxDecoration(
-                          color: AppColors.primary,
-                          shape: BoxShape.circle,
+
+                // content
+                Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    spacing: 6,
+                    children: [
+                      Text(
+                        product["name"]!,
+                        style: GoogleFonts.poppins(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
                         ),
-                        child: Icon(
-                          Icons.add,
-                          color: Colors.white,
-                          size: 18,
-                        ),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
                       ),
-                    ),
-                  ],
+                      Row(
+                        children: [
+                          Icon(Icons.timer_outlined, color: Colors.black, size: 14),
+                          Text(
+                            product["timer"]!,
+                            style: GoogleFonts.poppins(
+                              fontSize: 12,
+                              color: Colors.black,
+                            ),
+                          ),
+                          SizedBox(width: 4),
+                          Container(
+                            width: 1, // 👈 thin width = vertical line
+                            height: 10, // 👈 controls how tall the line is
+                            color: Colors.grey,
+                          ),
+                          SizedBox(width: 4),
+                          Text(
+                            product["category"]!,
+                            style: GoogleFonts.poppins(
+                              fontSize: 12,
+                              color: AppColors.textDark,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            product["price"]!,
+                            style: GoogleFonts.poppins(
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                              color: AppColors.textDark,
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {},
+                            child: Container(
+                              width: 30,
+                              height: 30,
+                              decoration: BoxDecoration(
+                                color: AppColors.primary,
+                                shape: BoxShape.circle,
+                              ),
+                              child: Icon(
+                                Icons.add,
+                                color: AppColors.background,
+                                size: 18,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
           ),
-        ],
-      ),
+        ),
+        Positioned(
+          top: 8, // 👈 distance from top
+          right: 8, // 👈 distance from right
+          child: GestureDetector(
+            onTap: () {
+              // toggle favourite
+            },
+            child: Row(
+              children: [
+                Icon(
+                  Icons.star, // or Icons.star_border for empty star
+                  color: Colors.amber,
+                  size: 18,
+                ),
+                Text("4.9", style: GoogleFonts.poppins(fontSize: 12)),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -39,9 +39,52 @@ class _HomeScreenState extends State<HomeScreen> {
     {
       "name": "Jollof Rice",
       "category": "Local Food",
-     "price": "₦1,800",
-    "image": "assets/images/plate_of_rice.png",
-    }
+      "price": "₦1,800",
+      "image": "assets/images/plate_of_rice.png",
+      "timer": "10min",
+    },
+    {
+      "name": "Jollof Rice",
+      "category": "Local Food",
+      "price": "₦1,800",
+      "image": "assets/images/plate_of_rice.png",
+      "timer": "10min",
+    },
+    {
+      "name": "Jollof Rice",
+      "category": "Local Food",
+      "price": "₦1,800",
+      "image": "assets/images/plate_of_rice.png",
+      "timer": "10min",
+    },
+    {
+      "name": "Jollof Rice",
+      "category": "Local Food",
+      "price": "₦1,800",
+      "image": "assets/images/plate_of_rice.png",
+      "timer": "10min",
+    },
+    {
+      "name": "Jollof Rice",
+      "category": "Local Food",
+      "price": "₦1,800",
+      "image": "assets/images/plate_of_rice.png",
+      "timer": "10min",
+    },
+    {
+      "name": "Jollof Rice",
+      "category": "Local Food",
+      "price": "₦1,800",
+      "image": "assets/images/plate_of_rice.png",
+      "timer": "10min",
+    },
+    {
+      "name": "Jollof Rice",
+      "category": "Local Food",
+      "price": "₦1,800",
+      "image": "assets/images/plate_of_rice.png",
+      "timer": "10",
+    },
   ];
 
   @override
@@ -57,194 +100,202 @@ class _HomeScreenState extends State<HomeScreen> {
         color: AppColors.background,
         padding: EdgeInsets.all(16),
         child: SafeArea(
-          child: Column(
-            spacing: 20,
-            children: [
-              SizedBox(
-                width: double.infinity,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(
-                      child: Row(
-                        children: [
-                          CircleAvatar(
-                            backgroundImage: AssetImage(
-                              "assets/images/user.png",
-                            ),
-                            radius: 20,
-                          ),
-                          SizedBox(width: 8),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment
-                                .start, // 👈 align text to left
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Text(
-                                "Hello Timolek",
-                                style: GoogleFonts.poppins(
-                                  color: AppColors.textDark,
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              GestureDetector(
-                                onTap: () {},
-                                child: Row(
-                                  children: [
-                                    Icon(
-                                      Icons.location_on,
-                                      color: Colors.red,
-                                      size: 14,
-                                    ),
-                                    SizedBox(width: 2),
-                                    Text(
-                                      "Mark Hostel",
-                                      style: TextStyle(fontSize: 12),
-                                    ),
-                                    Transform.rotate(
-                                      angle:
-                                          -1.5708, // 90 degrees in radians (rotates chevron left → points down)
-                                      child: Icon(
-                                        Icons.chevron_left,
-                                        size: 20,
-                                        color: Colors.black,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-
-                    Container(
-                      width: 50,
-                      height: 50, // must equal width for a perfect circle
-                      decoration: BoxDecoration(
-                        color: AppColors.primary,
-                        shape: BoxShape.circle, // 👈 easiest way
-                      ),
-                      child: Icon(
-                        Icons.notifications_outlined,
-                        size: 24,
-                        color: AppColors.background,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-
-              SizedBox(
-                width: double.infinity,
-                child: TextField(
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(
-                      Icons.search_outlined,
-                      color: AppColors.textDark,
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(22),
-                    ),
-                    floatingLabelStyle: TextStyle(
-                      color: Colors.red,
-                    ), // when active
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red),
-                      borderRadius: BorderRadius.circular(22),
-                    ),
-                    hintText: "Search for Food",
-                  ),
-                ),
-              ),
-
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Recommended",
-                    style: GoogleFonts.poppins(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: SingleChildScrollView(
+            child: Column(
+              spacing: 20,
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  child: Row(
                     crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      _buildCategory("Pizza", "assets/images/pizza.png", 1.5),
-                      _buildCategory(
-                        "Rice",
-                        "assets/images/plate_of_rice.png",
-                        0.8,
+                      SizedBox(
+                        child: Row(
+                          children: [
+                            CircleAvatar(
+                              backgroundImage: AssetImage(
+                                "assets/images/user.png",
+                              ),
+                              radius: 20,
+                            ),
+                            SizedBox(width: 8),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment
+                                  .start, // 👈 align text to left
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(
+                                  "Hello Timolek",
+                                  style: GoogleFonts.poppins(
+                                    color: AppColors.textDark,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                GestureDetector(
+                                  onTap: () {},
+                                  child: Row(
+                                    children: [
+                                      Icon(
+                                        Icons.location_on,
+                                        color: Colors.red,
+                                        size: 14,
+                                      ),
+                                      SizedBox(width: 2),
+                                      Text(
+                                        "Mark Hostel",
+                                        style: TextStyle(fontSize: 12),
+                                      ),
+                                      Transform.rotate(
+                                        angle:
+                                            -1.5708, // 90 degrees in radians (rotates chevron left → points down)
+                                        child: Icon(
+                                          Icons.chevron_left,
+                                          size: 20,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
-                      _buildCategory("Snacks", "assets/images/snacks.png", 1.5),
-                      _buildCategory(
-                        "Chicken",
-                        "assets/images/chicken.png",
-                        1.5,
+
+                      Container(
+                        width: 50,
+                        height: 50, // must equal width for a perfect circle
+                        decoration: BoxDecoration(
+                          color: AppColors.primary,
+                          shape: BoxShape.circle, // 👈 easiest way
+                        ),
+                        child: Icon(
+                          Icons.notifications_outlined,
+                          size: 24,
+                          color: AppColors.background,
+                        ),
                       ),
                     ],
                   ),
-                ],
-              ),
+                ),
 
-              // Add Carousel
-              Column(
-                children: [
-                  SizedBox(
-                    height: 180,
-                    child: PageView.builder(
-                      controller: _controller,
-                      itemCount: banners.length,
-                      onPageChanged: (index) {
-                        setState(() => _currentIndex = index);
-                      },
-                      itemBuilder: (context, index) {
-                        final item = banners[index];
-                        return _buildBannerCard(item);
-                      },
+                SizedBox(
+                  width: double.infinity,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.search_outlined,
+                        color: AppColors.textDark,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(22),
+                      ),
+                      floatingLabelStyle: TextStyle(
+                        color: Colors.red,
+                      ), // when active
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.red),
+                        borderRadius: BorderRadius.circular(22),
+                      ),
+                      hintText: "Search for Food",
                     ),
                   ),
-                  SizedBox(height: 10),
-                  // dots indicator
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: List.generate(banners.length, (index) {
-                      return AnimatedContainer(
-                        duration: Duration(milliseconds: 300),
-                        margin: EdgeInsets.symmetric(horizontal: 4),
-                        width: _currentIndex == index ? 20 : 8,
-                        height: 8,
-                        decoration: BoxDecoration(
-                          color: _currentIndex == index
-                              ? AppColors.primary
-                              : Colors.grey.shade300,
-                          borderRadius: BorderRadius.circular(4),
-                        ),
-                      );
-                    }),
-                  ),
-                ],
-              ),
+                ),
 
-              GridView.count(
-                crossAxisCount: 2,
-                crossAxisSpacing: 12,
-                mainAxisSpacing: 12,
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                children: products.map((product) {
-                  return ProductCard(
-                    product: product,
-                  ); // 👈 use it like any widget
-                }).toList(),
-              ),
-            ],
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Recommended",
+                      style: GoogleFonts.poppins(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        _buildCategory("Pizza", "assets/images/pizza.png", 1.5),
+                        _buildCategory(
+                          "Rice",
+                          "assets/images/plate_of_rice.png",
+                          0.8,
+                        ),
+                        _buildCategory(
+                          "Snacks",
+                          "assets/images/snacks.png",
+                          1.5,
+                        ),
+                        _buildCategory(
+                          "Chicken",
+                          "assets/images/chicken.png",
+                          1.5,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+
+                // Add Carousel
+                Column(
+                  children: [
+                    SizedBox(
+                      height: 180,
+                      child: PageView.builder(
+                        controller: _controller,
+                        itemCount: banners.length,
+                        onPageChanged: (index) {
+                          setState(() => _currentIndex = index);
+                        },
+                        itemBuilder: (context, index) {
+                          final item = banners[index];
+                          return _buildBannerCard(item);
+                        },
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    // dots indicator
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: List.generate(banners.length, (index) {
+                        return AnimatedContainer(
+                          duration: Duration(milliseconds: 300),
+                          margin: EdgeInsets.symmetric(horizontal: 4),
+                          width: _currentIndex == index ? 20 : 8,
+                          height: 8,
+                          decoration: BoxDecoration(
+                            color: _currentIndex == index
+                                ? AppColors.primary
+                                : Colors.grey.shade300,
+                            borderRadius: BorderRadius.circular(4),
+                          ),
+                        );
+                      }),
+                    ),
+                  ],
+                ),
+
+                GridView.count(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 12,
+                  mainAxisSpacing: 12,
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  childAspectRatio:
+                      0.70, // 👈 lower = taller cards, higher = shorter cards
+                  children: products.map((product) {
+                    return ProductCard(
+                      product: product,
+                    ); // 👈 use it like any widget
+                  }).toList(),
+                ),
+              ],
+            ),
           ),
         ),
       ),
